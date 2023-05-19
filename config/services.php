c967390e874c4271d395db52fb99628cbeae08d3
tim.messaoudene <tim.messaoudene@synolia.com>
2023-05-19 17:55:19 +0200

    Sylius account creation from google user Migration file Oauth connexion button

--- config/services.php
+++ config/services.php
@@ -1,9 +1,16 @@
 <?php
-
-declare(strict_types=1);
-
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
-return static function (ContainerConfigurator $configurator) {
-    $configurator->import('services/*');
+return function(ContainerConfigurator $containerConfigurator) {
+    $services = $containerConfigurator->services()
+        ->defaults()
+        ->autowire()      // Automatically injects dependencies in your services.
+        ->autoconfigure() // Automatically registers your services as commands, event subscribers, etc.
+    ;
+
+    $services->load('Synolia\SyliusAdminOauthPlugin\\', '../src/*')
+             ->exclude('../src/{DependencyInjection,Entity,Kernel.php}')
+             ->load('Synolia\SyliusAdminOauthPlugin\Controller\\', '../src/Controller')
+             ->tag('controller.service_arguments')
+    ;
 };
