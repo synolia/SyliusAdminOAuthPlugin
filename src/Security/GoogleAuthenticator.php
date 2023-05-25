2740d3fa609651c1787fc31ec282e1e289b3a496
tim.messaoudene <tim.messaoudene@synolia.com>
2023-05-25 17:10:38 +0200

    Oauth authentication handled

--- src/Security/GoogleAuthenticator.php
+++ src/Security/GoogleAuthenticator.php
@@ -4,12 +4,10 @@ declare(strict_types=1);
 
 namespace Synolia\SyliusAdminOauthPlugin\Security;
 
-use App\Entity\User\AdminUser;
 use Doctrine\ORM\EntityManagerInterface;
 use KnpU\OAuth2ClientBundle\Client\ClientRegistry;
 use KnpU\OAuth2ClientBundle\Security\Authenticator\OAuth2Authenticator;
 use League\OAuth2\Client\Provider\GoogleUser;
-use Sylius\Bundle\CoreBundle\Doctrine\ORM\UserRepository;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -20,7 +18,6 @@ use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
 use Symfony\Component\Security\Http\Authenticator\Passport\SelfValidatingPassport;
 use Synolia\SyliusAdminOauthPlugin\Service\UserCreationService;
-use Synolia\SyliusAdminOauthPlugin\Factory\AdminUserFactory;
 
 final class GoogleAuthenticator extends OAuth2Authenticator
 {
@@ -49,27 +46,10 @@ final class GoogleAuthenticator extends OAuth2Authenticator
         return new SelfValidatingPassport(
             new UserBadge($accessToken->getToken(), function () use ($googleUser, $email, $accessToken, $client) {
                 if (str_ends_with($email, "@synolia.com")) {
-                    /** @var UserRepository $userRepo */
-                    $userRepo = $this->entityManager->getRepository(AdminUser::class);
-                    $existingUser = $userRepo->findOneBy(['googleId' => $googleUser->getId()]);
-                    // 1) have they logged in with Google before? Easy!
-                    if ($existingUser) {
-                        return $existingUser;
-                    }
-                    // 2) do we have a matching user by email?
-                    $user = $this->entityManager->getRepository(AdminUser::class)->findOneBy(['email' => $email]);
-                    // 3) register google user
-                    if (!$user) {
-                        $user = AdminUserFactory::createByGoogleAccount($googleUser);
-//                    $oauthUser = UserOauthFactory::create($user);
-                        $this->entityManager->persist($user);
-//                    $this->entityManager->persist($oauthUser);
-                        $this->entityManager->flush();
-                    }
-                    return $user;
+                   return $this->userCreationService->createByGoogleAccount($googleUser);
                 }
                 else{
-                    throw new \Exception("Vous ne pouvez créer de compte administrateur.");
+                    throw new AuthenticationException("Vous ne pouvez créer de compte administrateur.");
                 }
             })
         );
@@ -95,27 +75,4 @@ final class GoogleAuthenticator extends OAuth2Authenticator
 
         return new Response($message, Response::HTTP_FORBIDDEN);
     }
-
-    /**
-     * Called when authentication is needed, but it's not sent.
-     * This redirects to the 'login'.
-     */
-    public function start(Request $request, AuthenticationException $authException = null): Response
-    {
-        return new RedirectResponse(
-            '/admin/login', // might be the site, where users choose their oauth provider
-            Response::HTTP_TEMPORARY_REDIRECT
-        );
-    }
-
-//    public function start(Request $request, AuthenticationException $authException = null): Response
-//    {
-//        /*
-//         * If you would like this class to control what happens when an anonymous user accesses a
-//         * protected page (e.g. redirect to /login), uncomment this method and make this class
-//         * implement Symfony\Component\Security\Http\EntryPoint\AuthenticationEntryPointInterface.
-//         *
-//         * For more details, see https://symfony.com/doc/current/security/experimental_authenticators.html#configuring-the-authentication-entry-point
-//         */
-//    }
 }
--- src/Service/UserCreationService.php
+++ src/Service/UserCreationService.php
@@ -6,7 +6,6 @@ namespace Synolia\SyliusAdminOauthPlugin\Service;
 
 use App\Entity\User\AdminUser;
 use Doctrine\ORM\EntityManagerInterface;
-use KnpU\OAuth2ClientBundle\Client\ClientRegistry;
 use League\OAuth2\Client\Provider\GoogleUser;
 use Sylius\Bundle\CoreBundle\Doctrine\ORM\UserRepository;
 use Synolia\SyliusAdminOauthPlugin\Factory\AdminUserFactory;
@@ -14,17 +13,18 @@ use Synolia\SyliusAdminOauthPlugin\Factory\AdminUserFactory;
 final class UserCreationService
 {
     public function __construct(
-        private ClientRegistry $clientRegistry,
         private EntityManagerInterface $entityManager,
-    ){}
-    public function createByGoogleAccount(GoogleUser $googleUser):AdminUser
+    ) {
+    }
+
+    public function createByGoogleAccount(GoogleUser $googleUser): AdminUser
     {
         /** @var UserRepository $userRepo */
         $userRepo = $this->entityManager->getRepository(AdminUser::class);
         /** @var AdminUser $existingUser */
         $existingUser = $userRepo->findOneBy(['googleId' => $googleUser->getId()]);
         // 1) have they logged in with Google before? Easy!
-        if (null !== $existingUser->getCreatedAt()) {
+        if (null !== $existingUser) {
             return $existingUser;
         }
         // 2) do we have a matching user by email?
