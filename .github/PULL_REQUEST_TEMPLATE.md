4e8e8ee6ea06bd8504491f8df5826496b9188681
Tim Messaoudene <44289224+timmess@users.noreply.github.com>
2023-06-29 08:58:00 +0200

    SYLIUS-277 : rebase main

---
+++ .github/PULL_REQUEST_TEMPLATE.md
@@ -0,0 +1,11 @@
+| Q             | A
+| ------------- | ---
+| Bug fix?      | yes/no
+| New feature?  | yes/no
+| BC breaks?    | yes/no
+| Fixed issue   | #... <!-- #-prefixed issue number(s), if any -->
+| License       | MIT
+
+<!--
+- Replace this comment by a description of what your PR is solving.
+-->
---
+++ DependencyInjection/SynoliaSyliusAdminOauthExtension.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Synolia\SyliusAdminOauthPlugin\DependencyInjection;
+
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\FileLoader;
+use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
+
+/**
+ * @param array $configs
+ * @param ContainerBuilder $container
+ *
+ * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+ * @SuppressWarnings(PHPMD.UnusedLocalVariable)
+ */
+final class SynoliaSyliusAdminOauthExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container): void
+    {
+        $config = $this->processConfiguration($this->getConfiguration([], $container), $configs);
+        $loader = new PhpFileLoader($container, new FileLocator(__DIR__ . '/../../config'));
+
+        $loader->load('services.php');
+    }
+
+    public function getConfiguration(array $config, ContainerBuilder $container): ConfigurationInterface
+    {
+        return new Configuration();
+    }
+}
